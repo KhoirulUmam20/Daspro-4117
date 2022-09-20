7c585af59797004161db837fa0ea343fedcc3a22
M. KHOIRUL UMAM <112909511+KhoirulUmam20@users.noreply.github.com>
2022-09-20 20:12:26 +0700

    TUGAS LUAS LINGKARAN PERT.5

---
+++ pertemuan-5/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    //CARA PERTAMA//
+    float a;
+    a = 3.14;
+    float b;
+    b = 5;
+    float hasil;
+    cout << a * b * b<< endl;
+
+
+    //CARA KEDUA//
+    float x;
+    x = 3.14;
+    float y;
+    y = 5;
+    float luas;
+    luas = x * y * y;
+    cout << "LUAS LINGKARAN  : " << luas << endl;
+
+
+    return 0;
+}
