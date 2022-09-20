13def2253be9c489704543136b7085f722e51969
M. KHOIRUL UMAM <112909511+KhoirulUmam20@users.noreply.github.com>
2022-09-20 20:11:19 +0700

    PERTEMUAN KE-3

---
+++ Pertemuan3/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int a= 4117;
+    cout << "\nNAMA           :M. KHOIRUL UMAM\nKELAS          :" << a;
+    // KOMENTAR XD //
+    float b= 171.5;
+    char  c= 'M';
+    string d= "laki-laki";
+    cout << "\nTinggi Badan   :"  << b;
+    cout << "\nSize Baju      :" << c;
+    cout << "\nJenis Kelamin  :" << d;
+    //cin dan scanf//
+    string u;
+    char huruf;
+    cout <<"\nstill alone    :" ; cin >> u;
+    printf ("hari pertama minggu ini  :");
+    scanf ("%c",&huruf);
+
+
+    return 0;
+}
